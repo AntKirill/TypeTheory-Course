3e1233016e73e0d5fa773d30a32ede26bbc2582c
kirant <kirant9797@gmail.com>
2017-09-01 10:55:54 +0300

    reduction without memoization

--- realisations/hw1_reduction.ml
+++ realisations/hw1_reduction.ml
@@ -6,6 +6,12 @@ struct
 	let compare = String.compare
 end);;
 
+module StringMap = Map.Make (
+struct
+	type t = string
+	let compare = String.compare
+end);;
+
 (* Вернуть список имён свободных переменных *)
 let free_vars l = 
 	let rec set_of_free_vars l = 
@@ -44,19 +50,129 @@ let free_to_subst term l var_name =
 			| Abs (x, n) -> dfs n (StringSet.add x bl)
 	in
 	if (dfs l StringSet.empty <> -1) then true
-	else false
-;;
+	else false;;
 
 (* Проверить, альфа-эквивалентны ли лямбда-выражения *)
-let is_alpha_equivalent a b = failwith "Not implemented yet";;
+let is_alpha_equivalent a b = 
+	let is_same var1 var2 mp1 mp2 = 
+		let get_mapped x mp =
+			if (StringMap.mem x mp) then StringMap.find x mp
+			else x
+		in
+		(get_mapped var1 mp1) = (get_mapped var2 mp2)
+	in
+	let update_with_fresh_var var new_var mp =
+		if (StringMap.mem var mp) then mp
+		else StringMap.add var new_var mp
+	in
+	let rec dfs l1 l2 mp1 mp2 =  
+		match (l1, l2) with
+			| (Var var1, Var var2) -> is_same var1 var2 mp1 mp2
+			| (App (p1, q1), App (p2, q2)) -> (dfs p1 p2 mp1 mp2) && (dfs q1 q2 mp1 mp2)
+			| (Abs (var1, p1), Abs (var2, p2)) -> 
+			dfs p1 p2 (update_with_fresh_var var1 ("_"^var1) mp1) (update_with_fresh_var var2 ("_"^var1) mp2)
+			| _ -> false
+
+	in
+	dfs a b StringMap.empty StringMap.empty;;
 
 (* Проверить, находится ли лямбда-выражение в нормальной форме *)
-let is_normal_form a = failwith "Not implemented yet";;
+let is_normal_form p = 
+	let rec inf p = 
+		let test_parts m n = 
+			match m with
+				| Abs(x, p) -> if (free_to_subst n p x) then false else inf p
+				| _ -> (inf m) && (inf n)
+		in
+		match p with
+			| App(m, n) -> test_parts m n
+			| Abs(x, m) -> inf m
+			| _ -> true
+	in inf p;;
+
+(* Проверить возможноесть подстановки. Параметры:
+что подставляем, где подставляем, вместо чего подставляем*)
+(* (\\var_name.l) term *)
+let try_to_subst term l var_name = 
+	let fv = StringSet.of_list (free_vars term) in
+	let subst_state = ref (-2) in
+	let rec dfs l bl = 
+		(*  1 - нашли вершину с правильным именем, свободную для подстановки
+			0 - ни у одной вершины в поддереве не совпало имя с var_name
+			-1 - нашил вершину с правильным именем, но она не свободна для подстановки *)
+		let update_subst_state i1 i2 = 
+			if (i1 = 1 || i2 = 1) then subst_state := 1 
+			else if (i1 = 0 && i2 = 0) then subst_state := 0
+			else subst_state := -1
+		in
+		let check_var x bl = 
+			let intersects s1 s2 = 
+				let s_inter = StringSet.inter s1 s2 in not (StringSet.is_empty s_inter)
+			in
+			if (x <> var_name) then (subst_state := 0; Var x)
+			else if (intersects fv bl) then (subst_state := -1; Var x)
+			else (subst_state := 1; term)
+		in
+		match l with 
+			| App (m, n) -> 
+				let new_left_term = dfs m bl in
+				let done_left = !subst_state in 
+				let new_right_term = dfs n bl in
+				let done_right = !subst_state in
+				update_subst_state done_left done_right;
+				App (new_left_term, new_right_term)
+			| Var x -> check_var x bl
+			| Abs (x, n) -> Abs (x, (dfs n (StringSet.add x bl)))
+	in
+	let new_term = dfs l StringSet.empty in
+	match !subst_state with 
+		| -1 -> (false, new_term)
+		| 0 -> (true, l)
+		| 1 -> (true, new_term)
+		| _ -> (false, new_term)
+;;
+
+let _normal_beta_reduction_impl a = 
+	let is_step_done = ref false in
+	let rec dfs term = 
+		let _app left right = 
+			(* (\\x.p) m *)
+			let part_case left right = 
+				let new_left = dfs left in 
+				if !is_step_done then App(new_left, right)
+				else let new_right = dfs right in
+				App (new_left, new_right)
+			in
+			let redex_case x p m = 
+				let (is_done, new_term) = try_to_subst m p x in
+				if is_done then (is_step_done := true; new_term)
+				else part_case p m 
+			in
+			match left with Abs (x, p) -> redex_case x p right
+				| _ -> part_case left right
+		in
+		match term with 
+			| App (m, n) -> _app m n
+			| Abs (x, m) -> Abs (x, (dfs m))
+			| t -> t
+	in
+	let new_term = dfs a in (!is_step_done, new_term)	
+;;
 
 (* Выполнить один шаг бета-редукции, используя нормальный порядок *)
-let normal_beta_reduction a = failwith "Not implemented yet";;
+let normal_beta_reduction a = 
+	let (is_step_done, new_term) = _normal_beta_reduction_impl a in
+	new_term
+;;
 
 (* Свести выражение к нормальной форме с использованием нормального
    порядка редукции; реализация должна быть эффективной: использовать 
    мемоизацию *)
-let reduce_to_normal_form a = failwith "Not implemented yet";;
+let reduce_to_normal_form a = 
+	let rec do_step a = 
+		let (is_step_done, new_term) = _normal_beta_reduction_impl a in
+		if is_step_done then do_step new_term
+		else a
+	in
+	do_step a
+;;
--- tests/t1_reduction.ml
+++ tests/t1_reduction.ml
@@ -1,24 +1,23 @@
 open Hw1_reduction;;
 open Hw1;;
 
-let test_all f = 
-	let tests = [
-		"\\x.y";
-		"\\x.x";
-		"(\\x.x x) \\y.y y";
-		"(\\x.x x) \\y.y y z";
-		"((((((((\\x.\\y.\\x.y)(\\x.x)(\\x.x)(\\a.y)b)))))))";
-		"(\\x.x)\\y.x";
-		"(\\x.x)(\\z.y)"
-	] in
-	let rec ta l = match l with
-		| hd :: tl -> f hd; ta tl
-		| [] -> print_string "Done\n"; print_newline()
-	in
-	ta tests
-;;
-
 let test_free_vars () = 
+	let test_all f = 
+		let tests = [
+			"\\x.y";
+			"\\x.x";
+			"(\\x.x x) \\y.y y";
+			"(\\x.x x) \\y.y y z";
+			"((((((((\\x.\\y.\\x.y)(\\x.x)(\\x.x)(\\a.y)b)))))))";
+			"(\\x.x)\\y.x";
+			"(\\x.x)(\\z.y)"
+		] in
+		let rec ta l = match l with
+			| hd :: tl -> f hd; ta tl
+			| [] -> print_string "Done\n"; print_newline()
+		in
+		ta tests
+	in
 	let print_list l =
 		let rec pl l = 
 			match l with
@@ -49,7 +48,138 @@ let test_free_to_subst () =
 	go "\\x.x" "\\x.x" "x";
 	go "\\y.x" "\\x.x" "x";
 	go "\\x.\\x.\\y.y" "\\x.x" "x";
-	go "\\x.\\x.\\y.y" "\\x.x" "zzzzzzzzz"
+	go "\\x.\\x.\\y.y" "\\x.x" "zzzzzzzzz";
+	go "p" "\\x.y" "x"
 ;;
 
-test_free_to_subst();;
+let test_is_alpha_equivalent () = 
+	let go s1 s2 = 
+		if Hw1_reduction.is_alpha_equivalent (lambda_of_string s1) (lambda_of_string s2) then print_string "True\n"
+		else print_string "False\n"
+	in
+	go "x" "y";
+	go "y" "y";
+	go "abc" "abc";
+	go "abc" "abcd";
+	go "\\x.x" "\\x.x";
+	go "\\x.x" "\\y.y";
+	go "\\x.x" "x x";
+	go "\\x.y" "\\x.z";
+	go "(\\x.y)" "(\\xxx.y)";
+	go "(\\x.y) (\\xx.y)" "(\\xxx.y) (\\xx.y)";
+	go "(\\x.y) (\\xx.y) (\\xxx.yyy)" "(\\xxx.y) (\\xx.y) (\\x.yyy)"
+;;
+
+let tests_for_reduction_1 = [
+	"x";	
+	"\\x.y";
+	"\\x.x";
+	"(\\x.x)(\\z.y)";
+	"(\\x.(\\x.x) (\\x.x)) (\\y.z)";
+	"(\\f.\\x.f (f (f (f x)))) (\\f.\\x.f (f (f (f x))))";
+	"(\\f.\\x.f (f (f (f (f x))))) (\\f.\\x.f (f (f (f (f x)))))";
+	"(\\x.x x x) (\\x.x) (\\x.x)";
+	"(\\x.x x) \\y.y y";
+	"(\\x.x x) \\y.y y z";
+	"((((((((\\x.\\y.\\x.y)(\\x.x)(\\x.x)(\\a.y)b)))))))";
+	"(\\x.x)\\y.x";
+	"x";
+	"\\x.x";
+	"(\\x.x) y";
+	"(\\x.\\y.x) y";
+	"x x x x x x x xxxxxxxxx";
+	"(\\x.x x) \\x.x x";
+	"(\\a.b) x";
+	"\\y.(\\a.b) x";
+	"(\\x.\\y.(\\a.b) x) y";
+	"(\\x.\\y.(\\a.\\x.a) x) y";
+	"(\\x.y) (\\xx.y) (\\xxx.yyy)";
+	"((\\x.(x) (x))) ((\\x.(x) (x)))"
+];;
+
+let tests_for_reduction_2 = [
+	"(\\x.x x x) (\\x.x) (\\x.x)";
+	"(\\x.x x x) ((\\x.x) (\\x.x))";
+	"(\\f.\\x.f (f (f (f x)))) (\\f.\\x.f (f (f (f x))))";
+	"(\\f.\\x.f (f (f (f (f x))))) (\\f.\\x.f (f (f (f (f x)))))";	
+	"(\\x.x x) (\\x.x x)";
+];;
+
+let test_reduction tests f = 
+	let apply f t =
+		f t;
+		print_string " for test: ";
+		print_string t;
+		print_newline()
+	in
+	let rec ta l = match l with
+		| hd :: tl -> apply f hd; ta tl
+		| [] -> print_string "Done\n"; print_newline()
+	in
+	ta tests;;
+
+let test_is_normal_form () = 
+	let go str = 
+		let a = lambda_of_string str in
+		if Hw1_reduction.is_normal_form a then print_string "True"
+		else print_string "False"
+	in
+	test_reduction tests_for_reduction_1 go;;
+
+let test_normal_beta_reduction () = 
+	let go str = 
+		let a = lambda_of_string str in
+		let reduced_l = Hw1_reduction.normal_beta_reduction a in
+		let str2 = string_of_lambda reduced_l in print_string str2
+	in
+	test_reduction tests_for_reduction_1 go;;
+
+let test_try_to_subst () = 
+	let go str_m str_n var_name = 
+		let g () = 
+			let f new_lambda = 
+				let str_lambda = string_of_lambda new_lambda in 
+				print_string "substituted OK, ";
+				print_string str_lambda;
+			in
+			let m = lambda_of_string str_m in
+			let n = lambda_of_string str_n in
+			let (substituted, new_lambda) = Hw1_reduction.try_to_subst m n var_name in
+			if substituted = false then print_string "NOT substituted "
+			else f new_lambda
+		in
+		g ();
+		print_string " For test: ";
+		print_string (str_m ^ ", " ^ str_n ^ ", " ^ var_name);
+		print_newline ()
+	in
+	go "x" "y" "y";
+	go "x" "\\y.y" "y";
+	go "\\x.x" "\\y.y" "y";
+	go "\\x.x" "\\x.y" "y";
+	go "\\x.x" "((\\a.a) (\\a.b)) (\\c.d)" "a";
+	go "\\x.x" "((\\a.a) (\\a.b)) (\\c.d)" "b";
+	go "\\x.x" "((\\a.a) (\\a.b)) (\\c.d)" "d";
+	go "\\x.x" "((\\a.d) (\\a.d)) (\\c.d)" "d";
+	go "\\x.x" "((\\a.a) (\\a.b)) (\\c.d)" "c";
+	go "x" "\\x.x" "x";
+	go "(\\x.x) x" "((\\x.a) (\\a.b)) (\\c.d)" "a";
+	go "y" "x x" "x";
+	go "(\\x.x) (\\x.x)" "x x x" "x"
+;;
+
+let test_reduce_to_normal_form () = 
+	let go str = 
+		let a = lambda_of_string str in
+		let reduced_l = Hw1_reduction.reduce_to_normal_form a in
+		let is_done = is_normal_form reduced_l in
+		let str2 = string_of_lambda reduced_l in 
+		match is_done with
+			| true -> print_string str2
+			| false -> print_string "NOT NORMAL FORM!!! "; print_string str2
+	in
+	test_reduction tests_for_reduction_2 go;;
+
+(* test_normal_beta_reduction ();; *)
+(* test_try_to_subst ();; *)
+test_reduce_to_normal_form();;
